a3db6b6a8d45a20d4af64337707fd349b5ad7de1
Alejandro Rosales <ja.rg@lasallistas.org.mx>
2023-11-08 16:52:35 +0000

    Add TresJS dependencies and 3D model to homepage.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
-<template>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ipsam. Totam dolores fuga debitis sint laudantium. Aut
-    illum tenetur deleniti pariatur, ipsum aperiam! Vel ratione molestias eaque culpa, deserunt a.
-</template>
-<script>
+<script setup lang="ts">
+const { nodes, } = await useGLTF('/models/scene.gltf')
+const modelRef = ref()
+
+watch(modelRef, model => {
+    model.position.set(0, 0, 0)
+})
 </script>
+
+<template>
+    <Suspense>
+        <TresCanvas clear-color="#82DBC5" shadows alpha power-preference="high-performance" preserve-drawing-buffer
+            physically-correct-lights>
+            <TresPerspectiveCamera :args="[45, 1, 0.1, 1000]" />
+            <OrbitControls />
+            <primitive :object="nodes.MyModel" />
+        </TresCanvas>
+    </Suspense>
+</template>
